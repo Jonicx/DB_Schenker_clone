d480a93ff7f0cd044714850b337416b20b546f54
Jonicx <havugimana.jonathan4@gmail.com>
2025-01-23 15:27:05 +0200

    Update modified files

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './style/styles.css';
-import './index.css'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/DB_Schenker_clone">
       <App />
     </Router>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DB_Schenker_clone/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/DB_Schenker_clone/',
+  build: {
+    rollupOptions: {
+      input: './index.html', // Explicitly define the input file
+    },
+  },
 });
